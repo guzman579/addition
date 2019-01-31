48c0c2c221a8f2f4c1561ddb2ff12afe63a02836
Irene <35508977+guzman579@users.noreply.github.com>
2019-01-30 20:52:12 -0800

    Create addition.cpp

---
+++ addition.cpp
@@ -0,0 +1,22 @@
+// addition.cpp
+// Displays the sum of two numbers.
+#include <iostream>
+using namespace std;
+
+int main()
+{
+      int number1; // first number to add
+      int number2; // second number to add
+      int sum; // sum of number1 and number2
+
+      cout<<"Enter first integer: "; // prompt 
+      cin>>number1; // read first number from user
+
+      cout<<"Enter second integer: "; // prompt 
+      cin>>number2; // read second number from user
+
+      sum = number1 + number2; // add numbers, then store total in sum
+
+      cout<<"Sum is "<<< sum << endl; // display sum
+   } // end main
+} // end class 
