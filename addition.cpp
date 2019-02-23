2b2da370449808919ec7bbcdb5d9d7f630aad247
Irene <35508977+guzman579@users.noreply.github.com>
2019-02-22 16:58:00 -0800

    Update addition.cpp

--- addition.cpp
+++ addition.cpp
@@ -5,18 +5,18 @@ using namespace std;
 
 int main()
 {
-      int number1; // first number to add
-      int number2; // second number to add
-      int sum; // sum of number1 and number2
+	int number1; // first number to add
+	int number2; // second number to add
+	int sum; // sum of number1 and number2
 
-      cout<<"Enter first integer: "; // prompt 
-      cin>>number1; // read first number from user
+	cout << "Enter first integer: "; // prompt 
+	cin >> number1; // read first number from user
 
-      cout<<"Enter second integer: "; // prompt 
-      cin>>number2; // read second number from user
+	cout << "Enter second integer: "; // prompt 
+	cin >> number2; // read second number from user
 
-      sum = number1 + number2; // add numbers, then store total in sum
+	sum = number1 + number2; // add numbers, then store total in sum
 
-      cout<<"Sum is "<<< sum << endl; // display sum
-   } // end main
-} // end class 
+	cout << "Sum is " <<  sum << endl; // display sum
+	system("pause");
+} // end main
